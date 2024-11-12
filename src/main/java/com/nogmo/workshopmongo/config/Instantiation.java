4dcc30ae2d5905e878c1de5a723080fc957be0ce
Henrique Santos <henriquersantos99@outlook.com>
2024-11-12 19:45:48 -0300

    Referencing the users posts

--- src/main/java/com/nogmo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nogmo/workshopmongo/config/Instantiation.java
@@ -41,6 +41,9 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, LocalDate.parse("23/03/2024", sdf), "Good morning", "I woke up happy today!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
