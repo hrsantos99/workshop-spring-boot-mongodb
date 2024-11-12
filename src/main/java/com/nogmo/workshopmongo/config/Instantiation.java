4d49ea1ab62274afc972b83eab3524ef3434f0fe
Henrique Santos <henriquersantos99@outlook.com>
2024-11-12 19:34:02 -0300

    Author data projection with DTO

--- src/main/java/com/nogmo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nogmo/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.nogmo.workshopmongo.domain.Post;
 import com.nogmo.workshopmongo.domain.User;
+import com.nogmo.workshopmongo.dto.AuthorDTO;
 import com.nogmo.workshopmongo.repository.PostRepository;
 import com.nogmo.workshopmongo.repository.UserRepository;
 
@@ -34,10 +35,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, LocalDate.parse("21/03/2024", sdf), "Vacation!", "I will travel to São Paulo.", maria);
-		Post post2 = new Post(null, LocalDate.parse("23/03/2024", sdf), "Good morning", "I woke up happy today!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, LocalDate.parse("21/03/2024", sdf), "Vacation!", "I will travel to São Paulo.", new AuthorDTO(maria));
+		Post post2 = new Post(null, LocalDate.parse("23/03/2024", sdf), "Good morning", "I woke up happy today!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
